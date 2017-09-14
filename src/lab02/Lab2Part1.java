ee8108531266f2fc5ab4591835795d0777dc8dd3
Manuel A. Alonso Tarajano <tarajano@gmail.com>
2017-09-14 12:16:58 -0400

    Initial commit.

---
+++ src/lab02/Lab2Part1.java
@@ -0,0 +1,14 @@
+package lab02;
+
+public class Lab2Part1 {
+
+	public Lab2Part1() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
