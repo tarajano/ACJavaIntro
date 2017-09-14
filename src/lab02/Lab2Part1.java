955cf3010730c0d41224f5f84d18e1f142f8903f
Manuel A. Alonso Tarajano <tarajano@gmail.com>
2017-09-14 16:40:37 -0400

    SUMMARY: Computes total seconds from hh::mm::ss and back to hh::mm:ss TODO: Accept input from stdin. Convert from dd::hh::mm::ss to seconds and back.
    FILE:   src/lab02/Lab2Part1.java
      ADDS: steps for conversion hh::mm::ss <-> seconds

--- src/lab02/Lab2Part1.java
+++ src/lab02/Lab2Part1.java
@@ -2,13 +2,22 @@ package lab02;
 
 public class Lab2Part1 {
 
-	public Lab2Part1() {
-		// TODO Auto-generated constructor stub
-	}
+	static int hours = 2;
+	static int minutes = 42;
+	static int seconds = 31;
+	static int minsPerHour = 60;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		System.out.printf("Time passed -> hours: %d, minutes: %d, seconds: %d\n", hours, minutes, seconds);
+		int totalSeconds = (hours * minsPerHour * minsPerHour) + (minutes * minsPerHour) + seconds;
+		System.out.println("  In seconds -> " + totalSeconds);
+		
+		hours = totalSeconds / 3600;
+		minutes = (totalSeconds % 3600) / minsPerHour;
+		seconds = (totalSeconds % 3600) % minsPerHour;
+		
+		System.out.printf("Back to hours -> hours: %d, minutes: %d, seconds: %d\n", hours, minutes, seconds);
 	}
 
 }
