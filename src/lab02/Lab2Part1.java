fd1ec9a1d8fdf0a44863d7d6453bac1f06daae8e
Manuel A. Alonso Tarajano <tarajano@gmail.com>
2017-09-14 22:42:40 -0400

    SUMMARY: Adds conversion from seconds to DAYs::HH::MM::SS.
    FILE:   src/lab02/Lab2Part1.java
      ADDS: Instructions to convert from seconds -> DAYS::HH::MM::SS.
      ADDS: Accepts input from STDIN.

--- src/lab02/Lab2Part1.java
+++ src/lab02/Lab2Part1.java
@@ -1,23 +1,46 @@
 package lab02;
 
+import java.util.Scanner;
+
 public class Lab2Part1 {
 
+	
+	static int days = 0;
 	static int hours = 2;
 	static int minutes = 42;
 	static int seconds = 31;
+	
+	static int secsPerMins = 60;
 	static int minsPerHour = 60;
-
+	static int hoursPerDay = 24;
+	static int secsPerHour = minsPerHour * secsPerMins;
+	static int secsPerDay = hoursPerDay * secsPerHour;
+	
 	public static void main(String[] args) {
 		
+		// Converting HH::MM::SS <--> SS
 		System.out.printf("Time passed -> hours: %d, minutes: %d, seconds: %d\n", hours, minutes, seconds);
-		int totalSeconds = (hours * minsPerHour * minsPerHour) + (minutes * minsPerHour) + seconds;
-		System.out.println("  In seconds -> " + totalSeconds);
+		seconds = (hours * minsPerHour * secsPerMins) + (minutes * secsPerMins) + seconds;
+		System.out.println("  In seconds -> " + seconds);
+		hours = seconds / secsPerHour;
+		minutes = (seconds % secsPerHour) / minsPerHour;
+		seconds = (seconds % secsPerHour) % secsPerMins;
+		System.out.printf("Back to hours -> %02d::%02d::%02d\n", hours, minutes, seconds);
 		
-		hours = totalSeconds / 3600;
-		minutes = (totalSeconds % 3600) / minsPerHour;
-		seconds = (totalSeconds % 3600) % minsPerHour;
+		// Scanning input from STDIN
+		Scanner myScanner = new Scanner(System.in);
+		System.out.printf("\n\nNumber of seconds (make it big): ");
+		seconds = myScanner.nextInt();
+		myScanner.close();
+		// Converting SS --> DD::HH::MM::SS
+		days = seconds / (secsPerHour * hoursPerDay);
+		hours = (seconds % ( secsPerHour * hoursPerDay)) / secsPerHour;
+		minutes = (seconds % secsPerHour) / minsPerHour;
+		seconds = (seconds % secsPerHour) % minsPerHour;
+		System.out.printf("days: %d, %02d::%02d::%02d\n", days, hours, minutes, seconds);
 		
-		System.out.printf("Back to hours -> hours: %d, minutes: %d, seconds: %d\n", hours, minutes, seconds);
+		
+		
 	}
 
 }
