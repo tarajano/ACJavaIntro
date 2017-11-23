e25e44c1107e01c4501bc1e9e369572c214cbd39
Manuel A. Alonso Tarajano <tarajano@gmail.com>
2017-11-23 13:57:46 -0500

    SUMMARY: Implements extra act 2.1, improves output readability.

--- src/Lab03Benchmark.java
+++ src/Lab03Benchmark.java
@@ -7,7 +7,6 @@
  * Nov 21, 2017  
  */
 public class Lab03Benchmark {
-	public static long timeStart;
 	public static long loopDurationMillisecs = 1000;
 	
 	public static void main(String[] args) {
@@ -16,10 +15,16 @@ public class Lab03Benchmark {
 		timedEmptyLoop();
 		
 		// Math.pow(x,y) loop
-		timedMathPowLoop();
+		timedMathPowLoop(3);
 		
 		// cube(x) loop
-		timedCubeLoop();
+		timedCubeLoop(3);
+
+		// Math.pow(x,y) loop
+		timedMathPowLoop(2);
+		
+		// square(x) loop
+		timedSquareLoop(2);
 		
 		// Control empty loop
 		timedEmptyLoop();
@@ -27,36 +32,49 @@ public class Lab03Benchmark {
 	
 	public static void timedEmptyLoop() {
 		int calls = 0;
-		timeStart = System.currentTimeMillis();
+		long timeStart = System.currentTimeMillis();
 		while ( System.currentTimeMillis() < timeStart + loopDurationMillisecs){
 			calls += 1;
+			calls = Math.abs(calls); // Nothing interesting here.
 		}
 		System.out.printf("Calls to empty loop: %d\n", calls);
 	}
 	
-	public static void timedMathPowLoop() {
+	public static void timedMathPowLoop(double var) {
 		int calls = 0;
-		timeStart = System.currentTimeMillis();
+		long timeStart = System.currentTimeMillis();
 		while ( System.currentTimeMillis() < timeStart + loopDurationMillisecs){
-			double res = Math.pow(4, 4);
+			double res = Math.pow(var, var);
 			calls += 1;
 		}
-		System.out.printf("Calls to Math.pow(): %d\n", calls);
+		System.out.printf("Calls to Math.pow(%.2f,%.2f): %d\n", var, var, calls);
 	}
 	
-	public static void timedCubeLoop() {
+	public static void timedCubeLoop(double var) {
 		int calls = 0;
-		timeStart = System.currentTimeMillis();
+		long timeStart = System.currentTimeMillis();
 		while ( System.currentTimeMillis() < timeStart + loopDurationMillisecs){
-			double result = cube(4);
-			double res = Math.pow(4, 4);
+			double result = cube(var);
 			calls += 1;
 		}
-		System.out.printf("Calls to cube():     %d\n", calls);
+		System.out.printf("Calls to cube(%.2f):     %d\n", var, calls);
 	}
 	
-	public static double cube (double n) {
+	public static void timedSquareLoop(double var) {
+		int calls = 0;
+		long timeStart = System.currentTimeMillis();
+		while ( System.currentTimeMillis() < timeStart + loopDurationMillisecs){
+			double result = square(var);
+			calls += 1;
+		}
+		System.out.printf("Calls to square(%.2f):   %d\n", var, calls);
+	}
+	
+	public static double cube(double n) {
 		return n * n * n;
 	}
-
+	
+	public static double square(double n) {
+		return n * n;
+	}
 }
