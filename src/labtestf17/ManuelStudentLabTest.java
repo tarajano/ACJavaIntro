faedc7a667bb5c6e7b2ab521ec97b1f300bf83f6
Manuel A. Alonso Tarajano <tarajano@gmail.com>
2018-01-11 11:35:59 -0500

    SUMMARY: Removed comments from a file Manuels.java
    REMARKS:
    TODO:
    
    FILE:
      ADDS:
      MODIFIES:
      REMOVES:
    
    FILE:
      ADDS:
      MODIFIES:
      REMOVES:

--- src/labtestf17/ManuelStudentLabTest.java
+++ src/labtestf17/ManuelStudentLabTest.java
@@ -16,9 +16,6 @@ public class ManuelStudentLabTest {
 		// TODO Auto-generated constructor stub
 	}
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		ItemAnalysis testGrade  = new ItemAnalysis();
